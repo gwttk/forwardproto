daadd7aba446710dbdef69b4dd17018bbf10baa4
Immueggpain <Immueggpain@users.noreply.github.com>
2019-04-23 19:57:31 +0800

    v1.1.5 for travis

--- src/main/java/com/github/immueggpain/smartproxy/Launcher.java
+++ src/main/java/com/github/immueggpain/smartproxy/Launcher.java
@@ -32,7 +32,7 @@ import org.apache.commons.cli.ParseException;
 
 public class Launcher {
 
-	private static final String VERSTR = "1.1.4";
+	private static final String VERSTR = "1.1.5";
 
 	public static class ClientSettings {
 		public String local_listen_ip;
